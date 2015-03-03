d008bd6cb1caef7ff1a5e061c0b75474acb6fcd7
Blake Niemyjski <bniemyjski@gmail.com>
2015-03-03 17:33:46 -0600

    Ensure that we are only processing the queue once on process exit.

--- Source/Platforms/Console/ExceptionlessConsoleExtensions.cs
+++ Source/Platforms/Console/ExceptionlessConsoleExtensions.cs
@@ -8,20 +8,43 @@
 #endregion
 
 using System;
+using Exceptionless.Dependency;
+using Exceptionless.Logging;
 
 namespace Exceptionless {
     public static class ExceptionlessConsoleExtensions {
+        private static EventHandler _onProcessExit;
+
         public static void Register(this ExceptionlessClient client) {
             client.Startup();
 
             // make sure that queued events are sent when the app exits
-            AppDomain.CurrentDomain.ProcessExit += (sender, args) => {
-                client.ProcessQueue();
-            };
+            client.RegisterOnProcessExitHandler();
         }
 
         public static void Unregister(this ExceptionlessClient client) {
             client.Shutdown();
+            client.UnregisterOnProcessExitHandler();
+        }
+
+        private static void RegisterOnProcessExitHandler(this ExceptionlessClient client) {
+            if (_onProcessExit == null)
+                _onProcessExit = (sender, args) => client.ProcessQueue();
+
+            try {
+                AppDomain.CurrentDomain.ProcessExit -= _onProcessExit;
+                AppDomain.CurrentDomain.ProcessExit += _onProcessExit;
+            } catch (Exception ex) {
+                client.Configuration.Resolver.GetLog().Error(typeof(ExceptionlessClientExtensions), ex, "An error occurred while wiring up to the process exit event.");
+            }
+        }
+
+        private static void UnregisterOnProcessExitHandler(this ExceptionlessClient client) {
+            if (_onProcessExit == null)
+                return;
+
+            AppDomain.CurrentDomain.ProcessExit -= _onProcessExit;
+            _onProcessExit = null;
         }
     }
 }
