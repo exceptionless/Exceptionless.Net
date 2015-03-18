769dc7bc2e8a77ddf0af9e29e4652ebf41273f8e
Blake Niemyjski <bniemyjski@gmail.com>
2015-03-18 17:51:28 -0500

    Fixed an issue where events wouldn't be submitted due to a large batch size.

--- Source/Shared/Submission/SubmissionResponse.cs
+++ Source/Shared/Submission/SubmissionResponse.cs
@@ -22,6 +22,7 @@ namespace Exceptionless.Submission {
             PaymentRequired = (HttpStatusCode)statusCode == HttpStatusCode.PaymentRequired;
             UnableToAuthenticate = (HttpStatusCode)statusCode == HttpStatusCode.Unauthorized || (HttpStatusCode)statusCode == HttpStatusCode.Forbidden;
             NotFound = (HttpStatusCode)statusCode == HttpStatusCode.NotFound;
+            RequestEntityTooLarge = (HttpStatusCode)statusCode == HttpStatusCode.RequestEntityTooLarge;
         }
 
         public bool Success { get; private set; }
@@ -30,6 +31,7 @@ namespace Exceptionless.Submission {
         public bool PaymentRequired { get; private set; }
         public bool UnableToAuthenticate { get; private set; }
         public bool NotFound { get; private set; }
+        public bool RequestEntityTooLarge { get; private set; }
 
         public int StatusCode { get; private set; }
         public string Message { get; private set; }
