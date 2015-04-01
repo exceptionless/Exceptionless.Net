514656af2dacd1f2b453a9b4b46eb7a0be0faab8
Blake Niemyjski <bniemyjski@gmail.com>
2015-04-01 16:21:07 -0500

    More cleanup

--- Source/Platforms/Web/NuGet/tools/uninstall.ps1
+++ Source/Platforms/Web/NuGet/tools/uninstall.ps1
@@ -1,11 +1,4 @@
-﻿# Copyright 2014 Exceptionless
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# 
-#      http://www.apache.org/licenses/LICENSE-2.0
-
-param($installPath, $toolsPath, $package, $project)
+﻿param($installPath, $toolsPath, $package, $project)
 
 Import-Module (Join-Path $toolsPath exceptionless.psm1)
 
