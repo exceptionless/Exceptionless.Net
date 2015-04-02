966b1a694d2006fb5997e09477bbba3cdeb95ddb
Kelly Elton <its.the.doc@gmail.com>
2015-04-02 12:04:41 -0500

    Added BufferingExceptionlessAppender

---
+++ Source/Platforms/Log4net/BufferingExceptionlessAppender.cs
@@ -0,0 +1,39 @@
+using System;
+using log4net.Appender;
+using log4net.Core;
+
+namespace Exceptionless.Log4net {
+    public class BufferingExceptionlessAppender : BufferingAppenderSkeleton {
+        private ExceptionlessClient _client = ExceptionlessClient.Default;
+
+        public string ApiKey { get; set; }
+        public string ServerUrl { get; set; }
+
+        protected override void SendBuffer(LoggingEvent[] events) {
+            foreach (var e in events) {
+                _client.SubmitFromLogEvent(e);
+            }
+        }
+
+        public override void ActivateOptions() {
+            base.ActivateOptions();
+            if (!String.IsNullOrEmpty(ApiKey) || !String.IsNullOrEmpty(ServerUrl))
+                _client = new ExceptionlessClient(config =>
+                {
+                    if (!String.IsNullOrEmpty(ApiKey))
+                        config.ApiKey = ApiKey;
+                    if (!String.IsNullOrEmpty(ServerUrl))
+                        config.ServerUrl = ServerUrl;
+                    config.UseInMemoryStorage();
+                });
+        }
+
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            if (!_client.Configuration.IsValid)
+                return;
+
+            base.Append(loggingEvent);
+        }
+    }
+}
