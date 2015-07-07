bb71ca5ee14d1536dc9b145bf6d363ecb4704317
ejsmith <eric@codesmithtools.com>
2015-07-06 21:28:08 -0500

    Change nlog and log4net to allow setting the min log level.

--- Source/Platforms/Log4net/Log4netExceptionlessLog.cs
+++ Source/Platforms/Log4net/Log4netExceptionlessLog.cs
@@ -4,26 +4,45 @@ using log4net;
 
 namespace Exceptionless.Log4net {
     public class Log4netExceptionlessLog : IExceptionlessLog {
-        // ignore and let NLog determine what should be captured.
+        public Log4netExceptionlessLog(LogLevel? minimumLogLevel = null) {
+            if (minimumLogLevel.HasValue)
+                MinimumLogLevel = minimumLogLevel.Value;
+        }
+
         public LogLevel MinimumLogLevel { get; set; }
 
         public void Error(string message, string source = null, Exception exception = null) {
+            if (LogLevel.Error < MinimumLogLevel)
+                return;
+
             LogManager.GetLogger(source ?? "Exceptionless").Error(message, exception);
         }
 
         public void Info(string message, string source = null) {
+            if (LogLevel.Info < MinimumLogLevel)
+                return;
+
             LogManager.GetLogger(source ?? "Exceptionless").Info(message);
         }
 
         public void Debug(string message, string source = null) {
+            if (LogLevel.Debug < MinimumLogLevel)
+                return;
+
             LogManager.GetLogger(source ?? "Exceptionless").Debug(message);
         }
 
         public void Warn(string message, string source = null) {
+            if (LogLevel.Warn < MinimumLogLevel)
+                return;
+
             LogManager.GetLogger(source ?? "Exceptionless").Warn(message);
         }
 
         public void Trace(string message, string source = null) {
+            if (LogLevel.Trace < MinimumLogLevel)
+                return;
+
             LogManager.GetLogger(source ?? "Exceptionless").Debug(message);
         }
 
--- Source/Platforms/NLog/NLogExceptionlessLog.cs
+++ Source/Platforms/NLog/NLogExceptionlessLog.cs
@@ -4,26 +4,45 @@ using NLog.Fluent;
 
 namespace Exceptionless.NLog {
     public class NLogExceptionlessLog : IExceptionlessLog {
-        // ignore and let NLog determine what should be captured.
+        public NLogExceptionlessLog(LogLevel? minimumLogLevel = null) {
+            if (minimumLogLevel.HasValue)
+                MinimumLogLevel = minimumLogLevel.Value;
+        }
+
         public LogLevel MinimumLogLevel { get; set; }
 
         public void Error(string message, string source = null, Exception exception = null) {
+            if (LogLevel.Error < MinimumLogLevel)
+                return;
+
             Log.Error().Message(message).LoggerName(source).Exception(exception).Write();
         }
 
         public void Info(string message, string source = null) {
+            if (LogLevel.Info < MinimumLogLevel)
+                return;
+
             Log.Info().Message(message).LoggerName(source).Write();
         }
 
         public void Debug(string message, string source = null) {
+            if (LogLevel.Debug < MinimumLogLevel)
+                return;
+
             Log.Debug().Message(message).LoggerName(source).Write();
         }
 
         public void Warn(string message, string source = null) {
+            if (LogLevel.Warn < MinimumLogLevel)
+                return;
+
             Log.Warn().Message(message).LoggerName(source).Write();
         }
 
         public void Trace(string message, string source = null) {
+            if (LogLevel.Trace < MinimumLogLevel)
+                return;
+
             Log.Trace().Message(message).LoggerName(source).Write();
         }
 
