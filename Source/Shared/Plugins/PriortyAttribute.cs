21c3e99511cc76c0a094935cc191d14fb723dc46
Blake Niemyjski <bniemyjski@gmail.com>
2015-08-24 13:21:31 -0500

    Fixes #36 Restrict the Priority attribute to classes

--- Source/Shared/Plugins/PriortyAttribute.cs
+++ Source/Shared/Plugins/PriortyAttribute.cs
@@ -4,6 +4,7 @@ namespace Exceptionless.Plugins {
     /// <summary>
     /// Used to determine plugin priority.
     /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
     public class PriorityAttribute : Attribute {
         public PriorityAttribute(int priority) {
             Priority = priority;
