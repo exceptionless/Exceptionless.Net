8c122fd5229e854d332ff5577936ee93ca5342f6
Eric Nagy <enagy@airware.com>
2015-08-31 10:55:15 -0700

    InMemorySubmissionClient changes reversed- didn't realize there were two versions of this file- applied changes to the incorrect file previously.

--- Source/Tests/Utility/InMemorySubmissionClient.cs
+++ Source/Tests/Utility/InMemorySubmissionClient.cs
@@ -1,33 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using Exceptionless;
 using Exceptionless.Models;
 using Exceptionless.Models.Data;
 using Exceptionless.Submission;
-using System.Net;
 
-namespace Exceptionless.SampleConsole
-{
-    public class InMemorySubmissionClient: ISubmissionClient {
-        private static readonly Dictionary<string, object> _eventRepository = new Dictionary<string, object>();
-        private static readonly Dictionary<string, object> _userDescriptionRepository = new Dictionary<string, object>();
+namespace Exceptionless.Tests.Utility {
+    public class InMemorySubmissionClient : ISubmissionClient {
+        public InMemorySubmissionClient() {
+            Events = new List<Event>();
+        }
+
+        public List<Event> Events { get; private set; } 
 
         public SubmissionResponse PostEvents(IEnumerable<Event> events, ExceptionlessConfiguration config, IJsonSerializer serializer) {
-            foreach (Event e in events)
-            {
-                string data = serializer.Serialize(e);
-                string referenceId = !string.IsNullOrWhiteSpace(e.ReferenceId)
-                    ? e.ReferenceId
-                    : Guid.NewGuid().ToString("D");
-                _eventRepository[referenceId] = data;
-            }
-            return new SubmissionResponse(HttpStatusCode.OK);
+            var data = events.ToList();
+            data.ForEach(e => {
+                if (e.Date == DateTimeOffset.MinValue)
+                    e.Date = DateTimeOffset.Now;
 
+                if (String.IsNullOrEmpty(e.Type))
+                    e.Type = Event.KnownTypes.Log;
+            });
+            Events.AddRange(data);
+
+            return new SubmissionResponse(202, "Accepted");
         }
 
         public SubmissionResponse PostUserDescription(string referenceId, UserDescription description, ExceptionlessConfiguration config, IJsonSerializer serializer) {
-            string data = serializer.Serialize(description);
-            _userDescriptionRepository[referenceId] = data;
-            return new SubmissionResponse(HttpStatusCode.OK);
+            var ev = Events.FirstOrDefault(e => e.ReferenceId == referenceId);
+            if (ev == null)
+                return new SubmissionResponse(404, "Not Found");
+
+            ev.Data[Event.KnownDataKeys.UserDescription] = description;
+
+            return new SubmissionResponse(200, "OK");
         }
 
         public SettingsResponse GetSettings(ExceptionlessConfiguration config, IJsonSerializer serializer) {
