beeb7903768133a2f6277cf6cbb9081a89ce4077
Blake Niemyjski <bniemyjski@gmail.com>
2015-11-23 17:29:10 -0600

    Fixed an issue where Data Exclusions wasn't being run on DefaultData

--- Source/Shared/Plugins/Default/ConfigurationDefaultsPlugin.cs
+++ Source/Shared/Plugins/Default/ConfigurationDefaultsPlugin.cs
@@ -8,7 +8,7 @@ namespace Exceptionless.Plugins.Default {
                 context.Event.Tags.Add(tag);
 
             foreach (var data in context.Client.Configuration.DefaultData)
-                context.Event.Data[data.Key] = data.Value;
+                context.Event.SetProperty(data.Key, data.Value, excludedPropertyNames: context.Client.Configuration.DataExclusions);
         }
     }
 }
