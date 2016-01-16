27f9373246353ff4c9cfba323e550cff98f0f026
Chris Santero <chris@solveitsimply.com>
2016-01-16 18:28:20 -0500

    Pass same ExceptionlessClient to WebApi exception filter
    This ensures that exceptions handled by the WebApi
    ExceptionlessHandleErrorAttribute are processed by the same
    ExceptionlessClient that was passed to RegisterWebApi.

--- Source/Platforms/WebApi/ExceptionlessHandleErrorAttribute.cs
+++ Source/Platforms/WebApi/ExceptionlessHandleErrorAttribute.cs
@@ -8,10 +8,20 @@ using TaskExtensions = Exceptionless.Threading.Tasks.TaskExtensions;
 
 namespace Exceptionless.WebApi {
     public class ExceptionlessHandleErrorAttribute : IExceptionFilter {
+        private readonly ExceptionlessClient _client;
+
         public bool HasWrappedFilter { get { return WrappedFilter != null; } }
 
         public IExceptionFilter WrappedFilter { get; set; }
         public bool AllowMultiple { get { return HasWrappedFilter && WrappedFilter.AllowMultiple; } }
+
+        public ExceptionlessHandleErrorAttribute() : this(ExceptionlessClient.Default) {
+            
+        }
+
+        public ExceptionlessHandleErrorAttribute(ExceptionlessClient client) {
+            _client = client;
+        }
 
         public virtual void OnHttpException(HttpActionExecutedContext actionExecutedContext, CancellationToken cancellationToken) {
             if (HasWrappedFilter)
@@ -22,7 +32,7 @@ namespace Exceptionless.WebApi {
             contextData.SetSubmissionMethod("ExceptionHttpFilter");
             contextData.Add("HttpActionContext", actionExecutedContext.ActionContext);
 
-            actionExecutedContext.Exception.ToExceptionless(contextData).Submit();
+            actionExecutedContext.Exception.ToExceptionless(contextData, client: _client).Submit();
         }
 
         public Task ExecuteExceptionFilterAsync(HttpActionExecutedContext actionExecutedContext, CancellationToken cancellationToken) {
