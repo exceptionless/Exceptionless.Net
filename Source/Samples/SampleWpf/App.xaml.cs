f5bd3cc2296eda08fd27e0997a3af03601ade740
Blake Niemyjski <bniemyjski@gmail.com>
2016-02-18 15:23:15 -0600

    Updated the sample wpf app

--- Source/Samples/SampleWpf/App.xaml.cs
+++ Source/Samples/SampleWpf/App.xaml.cs
@@ -5,6 +5,7 @@ namespace Exceptionless.SampleWpf {
     public partial class App : Application {
         private void Application_Startup(object sender, StartupEventArgs e) {
             ExceptionlessClient.Default.Configuration.UseTraceLogger();
+            ExceptionlessClient.Default.Configuration.UseSessions(useSessionIdManagement: true);
             ExceptionlessClient.Default.Register();
         }
     }
