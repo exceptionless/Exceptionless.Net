706a634be6cd4102e15e740dcf1b39bf4d890815
Sander Rijken <sr@d-90.nl>
2016-03-22 23:20:01 +0100

    Add benchmark to test different scenarios with generated exceptions

--- Source/Tests/Plugins/DeduplicationBenchmarks.cs
+++ Source/Tests/Plugins/DeduplicationBenchmarks.cs
@@ -6,6 +6,7 @@ using Exceptionless.Models;
 using Exceptionless.Plugins;
 using Exceptionless.Plugins.Default;
 using Exceptionless.Tests.Utility;
+using Xunit;
 
 namespace Exceptionless.Tests.Plugins {
     public class DeduplicationBenchmarks {
@@ -22,7 +23,8 @@ namespace Exceptionless.Tests.Plugins {
         }
 
         [Benchmark]
-        public void TestBenchmark1() {
+        [Fact]
+        public void LargeEventsFromFiles() {
             foreach (var ev in _events) {
                 var pluginContextData = new ContextData();
 
@@ -32,6 +34,39 @@ namespace Exceptionless.Tests.Plugins {
                     _errorPlugin.Run(context);
                     _duplicateCheckerPlugin.Run(context);
                 }
+            }
+        }
+
+        [Benchmark]
+        [Fact]
+        public void RandomExceptions() {
+            var builder = GetException(Guid.NewGuid().ToString()).ToExceptionless();
+            var context = new EventPluginContext(_client, builder.Target, builder.PluginContextData);
+
+            _errorPlugin.Run(context);
+            _duplicateCheckerPlugin.Run(context);
+        }
+
+        [Benchmark]
+        [Fact]
+        public void IdenticalExceptions()
+        {
+            var builder = GetException().ToExceptionless();
+            var context = new EventPluginContext(_client, builder.Target, builder.PluginContextData);
+
+            _errorPlugin.Run(context);
+            _duplicateCheckerPlugin.Run(context);
+        }
+
+        private Exception GetException(string message = "Test")
+        {
+            try
+            {
+                throw new Exception(message);
+            }
+            catch (Exception ex)
+            {
+                return ex;
             }
         }
     }
