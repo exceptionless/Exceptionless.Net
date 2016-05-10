b03572da4dcde725041ade3d97b5818c23163904
rBaz <bouazzarochdi@gmail.com>
2016-05-10 00:40:33 -0400

    refactoring
    refactoring interfaces, classes and some objects names.

---
+++ Source/Platforms/ExceptionlessCoreHandlerError.cs
@@ -0,0 +1,29 @@
+﻿using ExceptionLess.Core.Interfaces;
+using Microsoft.AspNet.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExceptionLess.Core
+{
+    public class ExceptionlessCoreHandlerError : IExceptionlessCoreErrorAttribute
+    {
+        
+        /// <summary>
+        /// Gets the context.
+        /// </summary>
+        /// <value>
+        /// The context.
+        /// </value>
+        public HttpContext Context { get; set; }
+
+        /// <summary>
+        /// Gets the exception.
+        /// </summary>
+        /// <value>
+        /// The exception.
+        /// </value>
+        public Exception Exception { get; set; }
+    }
+}
---
+++ Source/Platforms/ExceptionlessCoreOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExceptionLess.Core
+{
+    public class ExceptionlessCoreOptions
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether [re-throw exception].
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if [re-throw exception]; otherwise, <c>false</c>.
+        /// </value>
+        public bool RethrowException { get; set; }
+    }
+}
