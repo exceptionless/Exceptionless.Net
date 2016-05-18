95b4757eecda16b9cc6035e24e7309b39082658d
Blake Niemyjski <bniemyjski@gmail.com>
2016-05-18 11:07:48 -0500

    Everything builds and tests locally using the new project system.

---
+++ src/Samples/Exceptionless.SampleWebApi/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace Exceptionless.SampleWebApi {
+    public class Startup {
+        public void Configuration(IAppBuilder app) {
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(name: "DefaultApi", routeTemplate: "api/{controller}/{id}", defaults: new {
+                id = RouteParameter.Optional
+            });
+            app.UseWebApi(config);
+
+            ExceptionlessClient.Default.Configuration.UseTraceLogger();
+            ExceptionlessClient.Default.RegisterWebApi(config);
+        }
+    }
+
+    public class Program {
+        public static void Main(string[] args) {
+            string baseAddress = "http://localhost:9000/";
+
+            using (WebApp.Start<Startup>(url: baseAddress)) {
+                Console.WriteLine("Press any key to send a request...");
+                ConsoleKeyInfo key = Console.ReadKey();
+                while (key.KeyChar != 27) {
+                    // Create HttpCient and make a request to api/values 
+                    var client = new HttpClient();
+                    HttpResponseMessage response = client.GetAsync(baseAddress + "api/values").Result;
+
+                    Console.WriteLine(response);
+                    Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+
+                    key = Console.ReadKey();
+                }
+            }
+        }
+    }
+}
