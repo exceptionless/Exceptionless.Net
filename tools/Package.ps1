e9d4c20108dba44bb1530d9c9ba9c8cd0f9f8001
Blake Niemyjski <bniemyjski@gmail.com>
2016-05-23 16:27:40 -0500

    Updates to packaging

--- tools/Package.ps1
+++ tools/Package.ps1
@@ -7,64 +7,48 @@ Function Create-Directory([string] $directory_name) {
     }
 }
 
-Create-Directory $deploy_dir
+Create-Directory -Path $artifacts_dir
 
 ForEach ($p in $client_projects) {
+    If (Test-Path "$($p.SourceDir)\project.json") {
+        Write-Host "Building Client NuGet Package: $($p.Name)" -ForegroundColor Yellow
+        dotnet pack "$($p.SourceDir)" -c Release -o $artifacts_dir
+        Write-Host "Building Client NuGet Package: $($p.Name)" -ForegroundColor Yellow
+        
+        If (-not $?) {
+            $anyError = $True
+        }
+        
+        Continue;
+    }
+    
     $isSignedProject = $($p.Name).EndsWith(".Signed")
     $assemblyName = $($p.Name).Replace(".Signed", "")
     $workingDirectory = "$working_dir\$($p.Name)"
-    Create-Directory $workingDirectory
+    Create-Directory -Path $workingDirectory
 
     Write-Host "Building Client NuGet Package: $($p.Name)" -ForegroundColor Yellow
 
     #copy assemblies from build directory to working directory.
     ForEach ($b in $client_build_configurations) {
-        $isPclClient = ($($p.Name) -eq "Exceptionless.Portable") -or ($($p.Name) -eq "Exceptionless.Portable.Signed")
-        If (!$isPclClient -and ($($b.NuGetDir) -eq "portable-net40+sl50+win+wpa81+wp80")) {
-            Continue;
-        }
-
-        $isWebApi = ($($p.Name) -eq "Exceptionless.WebApi") -or ($($p.Name) -eq "Exceptionless.WebApi.Signed")
-        If ($isWebApi -and ($($b.TargetFrameworkVersionProperty) -ne "NET45")) {
-            Continue;
-        }
-        
         $buildDirectory = "$build_dir\$configuration\$($p.Name)\lib\$($b.NuGetDir)"
         $workingLibDirectory = "$workingDirectory\lib\$($b.NuGetDir)"
         Create-Directory $workingLibDirectory
-
-        # Work around until we are able to merge dependencies and update other project dependencies pre build (E.G., MVC client references Models)
-        Get-ChildItem -Path $buildDirectory | Where-Object { $_.Name -eq "$assemblyName.dll" -Or $_.Name -eq "$assemblyName.pdb" -or $_.Name -eq "$assemblyName.xml" } | Copy-Item -Destination $workingLibDirectory
-
-        If ($($p.MergeDependencies) -ne $null) {
-            ForEach ($assembly in $($p.MergeDependencies).Split(";", [StringSplitOptions]"RemoveEmptyEntries")) {
-                Get-ChildItem -Path $buildDirectory | Where-Object { $_.Name -eq "$assembly" -Or $_.Name -eq "$assembly".Replace(".dll", ".pdb") -or $_.Name -eq "$assembly".Replace(".dll", ".xml") } | Copy-Item -Destination $workingLibDirectory
-            }
-        }
     }
 
     # Copy the source code for Symbol Source.
     robocopy $($p.SourceDir) $workingDirectory\src\$($p.SourceDir.Replace($base_dir, """")) *.cs *.xaml /S /NP | Out-Null
-    robocopy "$base_dir\Source\Core" "$workingDirectory\src\Source\Core" *.cs /S /NP /XD obj | Out-Null
-    Copy-Item "$base_dir\Source\GlobalAssemblyInfo.cs" "$workingDirectory\src\Source\GlobalAssemblyInfo.cs"
-
-    If (($($p.Name) -ne "Exceptionless.Portable") -and ($($p.Name) -ne "Exceptionless.Portable.Signed")) {
-        robocopy "$base_dir\Source\Extras" "$workingDirectory\src\Source\Extras" *.cs /S /NP /XD obj | Out-Null
-    }
-
-    If ($($p.Name).StartsWith("Exceptionless.Mvc")) {
-        robocopy "$base_dir\Source\Platforms\Web" "$workingDirectory\src\Source\Platforms\Web" *.cs /S /NP /XD obj | Out-Null
-    }
+    Copy-Item "$base_dir\src\GlobalAssemblyInfo.cs" "$workingDirectory\src\src\GlobalAssemblyInfo.cs"
 
     If ((Test-Path -Path "$($p.SourceDir)\NuGet")) {
         Copy-Item "$($p.SourceDir)\NuGet\*" $workingDirectory -Recurse
     }
 
-    Copy-Item "$($source_dir)\Platforms\LICENSE.txt" "$workingDirectory"
-    Copy-Item "$($source_dir)\Shared\NuGet\tools\exceptionless.psm1" "$workingDirectory\tools"
+    Copy-Item "$($base_dir)\LICENSE.txt" "$workingDirectory"
+    Copy-Item "$($source_dir)\Exceptionless.Portable\NuGet\tools\exceptionless.psm1" "$workingDirectory\tools"
 
     $nuspecFile = "$workingDirectory\$($p.Name).nuspec"
-    If ($isSignedProject){
+    If ($isSignedProject) {
         Rename-Item -Path "$workingDirectory\$assemblyName.nuspec" -NewName $nuspecFile
     }
 
@@ -94,11 +78,8 @@ ForEach ($p in $client_projects) {
 
     $nuspec.Save($nuspecFile);
 
-    $packageDir = "$deploy_dir\packages"
-    Create-Directory $packageDir
-
 	$nuget_version = $env:APPVEYOR_BUILD_VERSION + $env:VERSION_SUFFIX
-    nuget pack $nuspecFile -OutputDirectory $packageDir -Version $nuget_version -Symbols
+    nuget pack $nuspecFile -OutputDirectory $artifacts_dir -Version $nuget_version -Symbols
 }
 
 Pop-Location
