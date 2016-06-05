3a47cca6015ba4743417c496648c2d16867e51ae
Blake Niemyjski <bniemyjski@gmail.com>
2016-06-04 23:12:52 -0500

    Ported the sample console app to .NET Core

--- src/Samples/Exceptionless.SampleConsole/Properties/AssemblyInfo.cs
+++ src/Samples/Exceptionless.SampleConsole/Properties/AssemblyInfo.cs
@@ -3,16 +3,11 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 //using Exceptionless.Configuration;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
 [assembly: AssemblyTitle("SampleConsole")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("2a45b7bb-82b6-4055-8c8e-1a3c81afb4f1")]
 
-[assembly: AssemblyVersion("2.0.*")]
 
 // example of setting an attribute value in config.
 //[assembly: ExceptionlessSetting("AttributeSettings", "AttributeValue")]
