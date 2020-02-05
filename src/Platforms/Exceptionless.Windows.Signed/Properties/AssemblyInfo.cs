e5c2d336237dbeae16c24989a9de65083bd2367d
Blake Niemyjski <bniemyjski@gmail.com>
2020-02-05 08:02:12 -0600

    Updated the build. Temp copied source into deprecated packages to simplify resource compiling... will be deleted in next release.

---
+++ src/Platforms/Exceptionless.Windows.Signed/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Exceptionless Windows")]
+[assembly: Guid("2d458cc4-3bb3-4852-b6a2-11d5ac8672df")]
