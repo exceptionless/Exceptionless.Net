9654f31a9b35881f1bb66ada7cae5d035b9c561f
WuYafeng <yafeng.wu@outlook.com>
2020-03-11 10:32:03 +0800

    Resolve conversation
    - Use 2.2 logging component
    - fix some typo
    - remove regions

--- src/Platforms/Exceptionless.Extensions.Logging/ExceptionlessLoggerExtensions.cs
+++ src/Platforms/Exceptionless.Extensions.Logging/ExceptionlessLoggerExtensions.cs
@@ -27,7 +27,7 @@ namespace Exceptionless {
         /// Adds Exceptionless to the logging pipeline using the <see cref="ExceptionlessClient.Default"/>.
         /// </summary>
         /// <param name="builder">The <see cref="ILoggingBuilder"/>.</param>
-        /// <param name="client">If a client is not specified then the <see cref="ExceptionlessClient.Default"/> wil be used.</param>
+        /// <param name="client">If a client is not specified then the <see cref="ExceptionlessClient.Default"/> will be used.</param>
         /// <returns>The <see cref="ILoggingBuilder"/>.</returns>
         public static ILoggingBuilder AddExceptionless(this ILoggingBuilder builder, ExceptionlessClient client = null) {
             builder.AddProvider(new ExceptionlessLoggerProvider(client ?? ExceptionlessClient.Default));
@@ -65,12 +65,11 @@ namespace Exceptionless {
             builder.AddProvider(new ExceptionlessLoggerProvider(configure));
             return builder;
         }
-        #region Obsolute
         /// <summary>
         /// Adds Exceptionless to the logging pipeline using the <see cref="ExceptionlessClient.Default"/>.
         /// </summary>
         /// <param name="factory">The <see cref="ILoggerFactory"/>.</param>
-        /// <param name="client">If a client is not specified then the <see cref="ExceptionlessClient.Default"/> wil be used.</param>
+        /// <param name="client">If a client is not specified then the <see cref="ExceptionlessClient.Default"/> will be used.</param>
         /// <returns>The <see cref="ILoggerFactory"/>.</returns>
         [Obsolete("Use ExceptionlessLoggerExtensions.AddExceptionless(ILoggingBuilder,ExceptionlessClient) instead.")]
         public static ILoggerFactory AddExceptionless(this ILoggerFactory factory, ExceptionlessClient client = null) {
@@ -113,6 +112,5 @@ namespace Exceptionless {
             factory.AddProvider(new ExceptionlessLoggerProvider(configure));
             return factory;
         } 
-        #endregion
     }
 }
