f31717442323a9283e64d5e560fc5a6ee94702f4
Blake Niemyjski <bniemyjski@gmail.com>
2020-09-29 08:54:05 -0500

    Removed Nancy: Nancy project is deprecated

--- src/Platforms/Exceptionless.Nancy/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: ComVisible(false)]
-[assembly: AssemblyTitle("Exceptionless.Nancy")]
-[assembly: AssemblyDescription("Exceptionless Nancy")]
-[assembly: Guid("8177e931-9496-4490-930c-130422d70a14")]
