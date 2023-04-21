9e24a9cf42d3b0f0cfbe0b6036eb89f24e3f5a98
Rolf Kristensen <snakefoot@users.noreply.github.com>
2023-04-21 15:10:57 +0200

    NLog ExceptionlessTarget: ProcessQueueAsync for ExceptionlessClient on Flush (#301)
    * ExceptionlessTarget - ProcessQueueAsync for ExceptionlessClient on Flush
    
    * ExceptionlessTarget - Added NLogInternalLoggger for easier troubleshooting

--- src/Platforms/Exceptionless.NLog/ExceptionlessTarget.cs
+++ src/Platforms/Exceptionless.NLog/ExceptionlessTarget.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using Exceptionless.Dependency;
 using NLog;
+using NLog.Common;
 using NLog.Config;
 using NLog.Layouts;
 using NLog.Targets;
@@ -29,14 +31,21 @@ namespace Exceptionless.NLog {
             var apiKey = RenderLogEvent(ApiKey, LogEventInfo.CreateNullEvent());
             var serverUrl = RenderLogEvent(ServerUrl, LogEventInfo.CreateNullEvent());
 
-            if (!String.IsNullOrEmpty(apiKey) || !String.IsNullOrEmpty(serverUrl))
+            if (!String.IsNullOrEmpty(apiKey) || !String.IsNullOrEmpty(serverUrl)) {
                 _client = new ExceptionlessClient(config => {
                     if (!String.IsNullOrEmpty(apiKey) && apiKey != "API_KEY_HERE")
                         config.ApiKey = apiKey;
                     if (!String.IsNullOrEmpty(serverUrl))
                         config.ServerUrl = serverUrl;
+                    config.UseLogger(new NLogInternalLoggger());
                     config.UseInMemoryStorage();
                 });
+            }
+            else {
+                if (_client.Configuration.Resolver.HasDefaultRegistration<Logging.IExceptionlessLog, Logging.NullExceptionlessLog>()) {
+                    _client.Configuration.UseLogger(new NLogInternalLoggger());
+                }
+            }
         }
 
         protected override void Write(LogEventInfo logEvent) {
@@ -61,6 +70,10 @@ namespace Exceptionless.NLog {
             }
 
             builder.Submit();
+        }
+
+        protected override void FlushAsync(AsyncContinuation asyncContinuation) {
+            _client.ProcessQueueAsync().ContinueWith(t => asyncContinuation(t.Exception));
         }
     }
 }
---
+++ src/Platforms/Exceptionless.NLog/NLogInternalLoggger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Exceptionless.Logging;
+using NLog.Common;
+
+namespace Exceptionless.NLog {
+    internal class NLogInternalLoggger : IExceptionlessLog {
+        public LogLevel MinimumLogLevel { get; set; }
+
+        public void Debug(string message, string source = null) {
+            InternalLogger.Debug("ExceptionLess: {0} Source={1}", message, source);
+        }
+
+        public void Error(string message, string source = null, Exception exception = null) {
+            if (exception is null)
+                InternalLogger.Error("ExceptionLess: {0} Source={1}", message, source);
+            else
+                InternalLogger.Error(exception, "ExceptionLess: {0} Source={1}", message, source);
+        }
+
+        public void Info(string message, string source = null) {
+            InternalLogger.Info("ExceptionLess: {0} Source={1}", message, source);
+        }
+
+        public void Trace(string message, string source = null) {
+            InternalLogger.Trace("ExceptionLess: {0} Source={1}", message, source);
+        }
+
+        public void Warn(string message, string source = null) {
+            InternalLogger.Warn("ExceptionLess: {0} Source={1}", message, source);
+        }
+
+        public void Flush() {
+            // NLog InternalLogger has no flush
+        }
+    }
+}
