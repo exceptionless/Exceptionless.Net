310680883096cc4e3c82766419ae370b684e136b
Blake Niemyjski <bniemyjski@gmail.com>
2025-04-07 20:10:45 -0500

    Upgraded to latest bootstrap

--- samples/Exceptionless.SampleMvc/App_Start/BundleConfig.cs
+++ samples/Exceptionless.SampleMvc/App_Start/BundleConfig.cs
@@ -17,7 +17,7 @@ namespace Exceptionless.SampleMvc {
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
