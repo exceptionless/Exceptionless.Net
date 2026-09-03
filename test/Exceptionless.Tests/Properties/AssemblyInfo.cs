b394d4afc60f397224731fdebd123a052b4f5519
Blake Niemyjski <bniemyjski@gmail.com>
2026-09-02 22:12:25 -0500

    chore: refresh September dependencies and test tooling

--- test/Exceptionless.Tests/Properties/AssemblyInfo.cs
+++ test/Exceptionless.Tests/Properties/AssemblyInfo.cs
@@ -1,3 +1,4 @@
-﻿using Xunit;
+﻿using Xunit.Sdk;
+using Xunit.v3;
 
-[assembly: CollectionBehavior(DisableTestParallelization = true, MaxParallelThreads = 1)]
+[assembly: Parallelization(Mode = ParallelMode.None)]
